58f8909ff9d1ed1a8db40a25cd9aa25f415b496d
Fanny Maille <fanny@mailles.fr>
2022-04-16 21:00:11 +0200

    Regroup createElement and create return btn

--- index.js
+++ index.js
@@ -1,91 +1,117 @@
 /**
  * Fonctionnalités des favoris :
- * 
+ *
  * 1. Créer un élément (a ou button) dans le dom avec data-favorite="SON_ID_UNIQUE" (SON_ID_UNIQUE est l'id du NFT renvoyé par l'api)
  * 2. Lui ajouter data-favorite-add-text="TEXTE_A_AFFICHER_POUR_AJOUTER" (exemple : "Ajouter aux favoris")
  * 3. Lui ajouter data-favorite-remove-text="TEXTE_A_AFFICHER_POUR_SUPPRIMER" (exemple : "Retirer des favoris")
  * 4. Si on veut ajouter du style css au bouton en fonction du statut, ajoutez les classes dynamiques :
  *  - data-favorite-added-classes="class1 class2" --> le bouton aura les classes si le nft est dans les fav
  *  - data-favorite-removed-classes="class1 class2" --> le bouton aura les classes si le nft n'est pas dans les fav
- * 
+ *
  */
 const initFavorites = () => {
+  const keyFavorites = "nftFavorites";
+  let favorites = JSON.parse(localStorage.getItem(keyFavorites));
+  if (!Array.isArray(favorites)) {
+    favorites = [];
+  }
 
-    const keyFavorites = 'nftFavorites'
-    let favorites = JSON.parse(localStorage.getItem(keyFavorites));
-    if(!Array.isArray(favorites)){
-        favorites = []
-    }
+  const updateState = (element) => {
+    document
+      .querySelectorAll(`[data-favorite="${element.dataset.favorite}"]`)
+      .forEach((button) => {
+        button.dataset.favoriteAdded = favorites.includes(
+          button.dataset.favorite
+        )
+          ? 1
+          : 0;
+      });
+  };
 
-    const updateState = (element) => {
-        document.querySelectorAll(`[data-favorite="${element.dataset.favorite}"]`).forEach((button) => {
-            button.dataset.favoriteAdded = favorites.includes(button.dataset.favorite) ? 1 : 0
-        })
-    }
+  const updateText = (element) => {
+    document
+      .querySelectorAll(`[data-favorite="${element.dataset.favorite}"]`)
+      .forEach((button) => {
+        button.innerHTML =
+          button.dataset.favoriteAdded == 1
+            ? button.dataset.favoriteAddText ?? ""
+            : button.dataset.favoriteRemoveText ?? "";
+      });
+  };
 
-    const updateText = (element) => {
-        document.querySelectorAll(`[data-favorite="${element.dataset.favorite}"]`).forEach((button) => {
-            button.innerHTML = button.dataset.favoriteAdded == 1 ? button.dataset.favoriteAddText ?? '' : button.dataset.favoriteRemoveText ?? ''
-        })
-    }
+  const updateClasses = (element) => {
+    document
+      .querySelectorAll(`[data-favorite="${element.dataset.favorite}"]`)
+      .forEach((button) => {
+        const addedClasses = button.dataset.favoriteAddedClasses
+          ? button.dataset.favoriteAddedClasses.split(" ")
+          : null;
+        const removedClasses = button.dataset.favoriteRemovedClasses
+          ? button.dataset.favoriteRemovedClasses.split(" ")
+          : null;
+        if (button.dataset.favoriteAdded == 1) {
+          if (addedClasses) {
+            button.classList.add(...addedClasses);
+          }
+          if (removedClasses) {
+            button.classList.remove(...removedClasses);
+          }
+        } else {
+          if (addedClasses) {
+            button.classList.remove(...addedClasses);
+          }
+          if (removedClasses) {
+            button.classList.add(...removedClasses);
+          }
+        }
+      });
+  };
 
-    const updateClasses = (element) => {
-        document.querySelectorAll(`[data-favorite="${element.dataset.favorite}"]`).forEach((button) => {
-            const addedClasses = button.dataset.favoriteAddedClasses ? button.dataset.favoriteAddedClasses.split(' ') : null
-            const removedClasses = button.dataset.favoriteRemovedClasses ? button.dataset.favoriteRemovedClasses.split(' ') : null
-            if(button.dataset.favoriteAdded == 1){
-                if(addedClasses){
-                    button.classList.add(...addedClasses)
-                }
-                if(removedClasses){
-                    button.classList.remove(...removedClasses)
-                }
-            } else {
-                if(addedClasses){
-                    button.classList.remove(...addedClasses)
-                }
-                if(removedClasses){
-                    button.classList.add(...removedClasses)
-                }
-            }
-        })
-    }
+  document.querySelectorAll("[data-favorite]").forEach((element) => {
+    const idNft = element.dataset.favorite;
+    updateState(element);
+    updateText(element);
+    updateClasses(element);
 
-    document.querySelectorAll('[data-favorite]').forEach((element) => {
-        const idNft = element.dataset.favorite;
-        updateState(element)
-        updateText(element)
-        updateClasses(element)
-
-        element.addEventListener('click', () => {
-            const indexOfNft = favorites.indexOf(idNft)
-            if(indexOfNft == -1){
-                favorites.push(idNft)
-            } else {
-                favorites.splice(indexOfNft,1)
-            }
-            updateState(element)
-            updateText(element)
-            updateClasses(element)
-            localStorage.setItem(keyFavorites, JSON.stringify(favorites))
-    
-        })
-    })
-
-    
+    element.addEventListener("click", () => {
+      const indexOfNft = favorites.indexOf(idNft);
+      if (indexOfNft == -1) {
+        favorites.push(idNft);
+      } else {
+        favorites.splice(indexOfNft, 1);
+      }
+      updateState(element);
+      updateText(element);
+      updateClasses(element);
+      localStorage.setItem(keyFavorites, JSON.stringify(favorites));
+    });
+  });
 };
 
 const initApp = () => {
-    initFavorites()
-}
+  initFavorites();
+};
 
-document.addEventListener('datasLoaded', () => {
-    initApp();
-})
+document.addEventListener("datasLoaded", () => {
+  initApp();
+});
 
 function createElement(tag, config, parent = null) {
-  const { text, imgsrc, myclass, color, bgcolor, border, myid, myonclick, 
-    dataFavorite, dataFavoriteText, dataFavoriteRemoveText, dataFavoriteAddClass, dataFavoriteRemoveClass } = config || {};
+  const {
+    text,
+    imgsrc,
+    myclass,
+    color,
+    bgcolor,
+    border,
+    myid,
+    myonclick,
+    dataFavorite,
+    dataFavoriteText,
+    dataFavoriteRemoveText,
+    dataFavoriteAddClass,
+    dataFavoriteRemoveClass,
+  } = config || {};
 
   const element = document.createElement(tag);
 
@@ -101,40 +127,43 @@ function createElement(tag, config, parent = null) {
     element.style.borderColor = border;
   }
 
-  if(dataFavorite){
-    element.setAttribute('data-favorite', dataFavorite);
-  }
-  
-  if(dataFavoriteText){
-    element.setAttribute('data-favorite-add-text', dataFavoriteText);
+  if (dataFavorite) {
+    element.setAttribute("data-favorite", dataFavorite);
   }
 
-  if(dataFavoriteRemoveText){
-    element.setAttribute('data-favorite-remove-text', dataFavoriteRemoveText);
+  if (dataFavoriteText) {
+    element.setAttribute("data-favorite-add-text", dataFavoriteText);
   }
 
-  if(dataFavoriteAddClass){
-    element.setAttribute('data-favorite-added-classes', dataFavoriteAddClass);
+  if (dataFavoriteRemoveText) {
+    element.setAttribute("data-favorite-remove-text", dataFavoriteRemoveText);
   }
 
-  if(dataFavoriteRemoveClass){
-    element.setAttribute('data-favorite-removed-classes', dataFavoriteRemoveClass);
+  if (dataFavoriteAddClass) {
+    element.setAttribute("data-favorite-added-classes", dataFavoriteAddClass);
+  }
+
+  if (dataFavoriteRemoveClass) {
+    element.setAttribute(
+      "data-favorite-removed-classes",
+      dataFavoriteRemoveClass
+    );
   }
 
   if (imgsrc) {
-    element.setAttribute('src', imgsrc);
+    element.setAttribute("src", imgsrc);
   }
 
   if (myclass) {
-    element.setAttribute('class', myclass);
+    element.setAttribute("class", myclass);
   }
 
   if (myid) {
-    element.setAttribute('id', myid);
+    element.setAttribute("id", myid);
   }
 
   if (myonclick) {
-    element.setAttribute('onclick', myonclick);
+    element.setAttribute("onclick", myonclick);
   }
 
   if (text) {
