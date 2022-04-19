b9d614c730145e5d94be3508d7dfdcd093a81afd
Axelle Blondin <axelle.blondin@polynom.io>
2022-04-19 21:34:57 +0200

    ADD : filters

--- index.js
+++ index.js
@@ -9,6 +9,20 @@
  *  - data-favorite-removed-classes="class1 class2" --> le bouton aura les classes si le nft n'est pas dans les fav
  *
  */
+
+/**
+ * Performs a GET method on the given URL with the fetch function.
+ * 
+ * @param {string} url - url to fetch from
+ * @returns json parsed from the fetched data.
+ */
+ async function dataFetching(url) {
+    const raw = await fetch(url);
+    const { assets } = await raw.json();
+    return assets;
+  }
+
+
 const initFavorites = () => {
   const keyFavorites = "nftFavorites";
   let favorites = JSON.parse(localStorage.getItem(keyFavorites));
@@ -88,29 +102,53 @@ const initFavorites = () => {
   });
 };
 
+ const initFilters = async () => {
+    const filterDiv = document.getElementsByClassName('filters')[0];
+    const data = await dataFetching("https://awesome-nft-app.herokuapp.com/")
+    let creatorNames = data.map((nft) => nft.creator.username );
+    creatorNames = creatorNames.filter((a, b) => creatorNames.indexOf(a) === b );
+    creatorNames = creatorNames.filter((e) => e.length !== 0);
+    const selector = filterDiv.querySelector('select');
+    selector.options.add(new Option( 'All', 'all creators', true, true ));
+    for ( const o of creatorNames ) {
+        selector.options.add(new Option( o ));
+    };
+    selector.addEventListener('change', async function() {
+        const data = await dataFetching(`https://awesome-nft-app.herokuapp.com/search?q=${ selector.value }`);
+        const newList = data.filter(( item ) => item.creator.username.toLowerCase().includes( selector.value.toLowerCase()))
+        getProducts( newList );
+    })
+}
+
 const initApp = () => {
   initFavorites();
 };
 
 document.addEventListener("datasLoaded", () => {
   initApp();
+  initFilters();
 });
+
 
 function createElement(tag, config, parent = null) {
   const {
-    text,
-    imgsrc,
-    myclass,
-    color,
     bgcolor,
     border,
-    myid,
-    myonclick,
+    color,
     dataFavorite,
-    dataFavoriteText,
-    dataFavoriteRemoveText,
     dataFavoriteAddClass,
     dataFavoriteRemoveClass,
+    dataFavoriteRemoveText,
+    dataFavoriteText,
+    imgsrc,
+    myclass,
+    myid,
+    myonclick,
+    myonkeyup,
+    myonchange,
+    placeholder,
+    text,
+    type
   } = config || {};
 
   const element = document.createElement(tag);
@@ -166,13 +204,115 @@ function createElement(tag, config, parent = null) {
     element.setAttribute("onclick", myonclick);
   }
 
+  if (myonkeyup) {
+    element.setAttribute("onkeyup", myonkeyup);
+  }
+
+  if (myonchange) {
+    element.onchange = myonchange;
+
+  }
+
+  if (placeholder) {
+    element.setAttribute("placeholder", placeholder);
+  }
+
   if (text) {
     element.innerHTML = text;
   }
+  if (type) {
+    element.setAttribute("type",type);
+  }
+
   if (!parent) {
     root.appendChild(element);
   } else {
     parent.appendChild(element);
   }
   return element;
+}
+
+async function getProducts( data ) {
+    const div = document.getElementsByClassName('allentities')[0];
+    while(div.firstChild){
+        div.removeChild(div.firstChild);
+    }
+    const allentities = document.getElementsByClassName('allentities')[0];
+    let n = 0;
+    for ( let i = 1 ; i < 6 ; i++ ) {
+        let assets = await dataFetching(`https://awesome-nft-app.herokuapp.com/?page=${i}`);
+        if ( data && Object.keys( data ).length > 0 ) {
+            assets = data;
+        }
+        assets.map(({ name, image_url, id, creator, collection, sales }) => {
+            //create Card
+            createElement('div', { myclass: 'card' }, allentities);
+            const card = document.getElementsByClassName('card')[n];
+
+            //create div header
+            createElement('div', { myclass: 'card-header' }, card);
+            const cardHeader = document.getElementsByClassName('card-header')[n];
+
+            //Create title and heart
+            createElement('div', { myclass: 'titlenft' }, cardHeader);
+            const titlenft = document.getElementsByClassName('titlenft')[n];
+            createElement('h2', { text: name, color: '#627264', myclass: 'namenft' }, titlenft);
+            createElement('a', {
+            myclass: 'favorite', dataFavorite: id,
+            dataFavoriteAddClass: 'favoriteadd', dataFavoriteRemoveClass: 'favoriteremove'
+            }, titlenft);
+
+            //create p username
+            if (creator.username) {
+            createElement('p', { text: `Created by ${creator.username} <br> On the ${collection.created_at.slice(0, 10)}`, myclass: 'card-subtitle text-gray' }, cardHeader);
+            }
+            else {
+            createElement('p', { text: `Created by unknown Creator <br> On the ${collection.created_at.slice(0, 10)}`, myclass: 'card-subtitle text-gray' }, cardHeader);
+            }
+
+            //create p sales
+            if (sales) {
+            createElement('p', { text: `${sales} sales`, color: '#627264', myclass: 'card-subtitle' }, cardHeader);
+            }
+            else {
+            createElement('p', { text: `0 sales`, color: '#627264', myclass: 'card-subtitle' }, cardHeader);
+            }
+
+            //create image
+            if (image_url) {
+                createElement('DIV', { myclass: 'card-image' }, card);
+                const cardImage = document.getElementsByClassName('card-image')[n];
+                createElement('img', { imgsrc: image_url, myclass: 'img-responsive image' }, cardImage);
+            }
+
+            //create btn
+            createElement('div', { myclass: 'card-footer' }, cardHeader);
+            const cardFooter = document.getElementsByClassName('card-footer')[n];
+            createElement('button', { text: "Voir la fiche détails", myonclick: `window.location.href='product.html?id=${id}'`, myclass: 'btn', color: '#627264', bgcolor: '#A1CDA8', border: '#A1CDA8' }, cardFooter);
+            n++;
+        })
+    }
+    
+
+}
+
+async function sortbySales(param) {
+    if ( param ) {
+        const data = await dataFetching(`https://awesome-nft-app.herokuapp.com/`);
+        const newList = data.sort(( item1, item2 ) => item2.sales - item1.sales );
+        getProducts( newList );
+    } else {
+        getProducts();
+    }
+}
+
+async function searchByKeyword() {
+    const filterDiv = document.getElementsByClassName('filters')[0];
+    const input = filterDiv.querySelector('input')
+    if ( input.value.toLowerCase().trim().length > 0 ) {
+        const data = await dataFetching(`https://awesome-nft-app.herokuapp.com/search?q=${ input.value.toLowerCase().trim() }`);
+        getProducts( data );
+    } else {
+        getProducts();
+    }
 }
